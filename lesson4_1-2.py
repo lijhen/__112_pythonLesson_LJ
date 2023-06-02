914624f36d68595d8c51368d47097a1a4d3243e0
lijhen <ob.lijhen@gmail.com>
2023-06-03 00:00:39 +0800

    modify

--- lesson4_1-2.py
+++ lesson4_1-2.py
@@ -1,6 +1,6 @@
 import requests
 import pandas as pd
-
+import streamlit as st
 
 url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
 response = requests.request('GET',url)
@@ -13,8 +13,8 @@ else:
 
 
 dataFrame = pd.DataFrame(data=all_data,columns=['sna','tot','sbi','sarea','mday','ar','bemp','act'])
-min = int(input('請輸入要查詢的可借數量：'))
-mask = dataFrame['sbi'] <= min
+
+mask = dataFrame['sbi'] <= 3
 mask_dataFrame = dataFrame[mask]
-filename = f'可借小於{min}的站點.xlsx'
-mask_dataFrame.to_excel(filename)
+
+st.dataframe(mask_dataFrame)
